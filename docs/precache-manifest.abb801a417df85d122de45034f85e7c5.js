a87e7b9663b241006649c011428e9eb1e74084fe
Oswaldo Jerez <ojerez@urbe.edu.ve>
2020-07-15 13:07:34 -0600

    Docs Agregado

---
+++ docs/precache-manifest.abb801a417df85d122de45034f85e7c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319734f4c4101089346046bab09a2fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6679955a8c99630a852d",
+    "url": "/static/css/main.54e6091f.chunk.css"
+  },
+  {
+    "revision": "8823992db4083bc910f0",
+    "url": "/static/js/2.15b632a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15b632a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6679955a8c99630a852d",
+    "url": "/static/js/main.2f9d562a.chunk.js"
+  },
+  {
+    "revision": "ef0e0127a75cdb20d5ea",
+    "url": "/static/js/runtime-main.183ce351.js"
+  }
+]);
